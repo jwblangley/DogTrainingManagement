f337a7d579b7c2adc0d7d406fbe73b4e8a64a9bd
James Langley <jwblangley@gmail.com>
2025-01-26 18:04:24 +0000

    Add desirable props to DataGrid

--- frontend/src/Clients.jsx
+++ frontend/src/Clients.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useContext } from 'react';
 
 import Typography from '@mui/material/Typography';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 
 import { BackendContext } from './BackendProvider';
 
@@ -11,17 +11,18 @@ export default function Clients() {
 
     const [users, setUsers] = useState([])
 
+    const [rowSelectionModel, setRowSelectionModel] = useState([]);
+
     useEffect(() => {
         backend.current.listClients().then(data => {
           setUsers(data)
         })
     }, [])
 
-
     const columns = [
         { field: 'first_name', headerName: 'First name', width: 150 },
         { field: 'last_name', headerName: 'Last name', width: 150 },
-        { field: 'email', headerName: 'Email Address', width: 150 },
+        { field: 'email', headerName: 'Email Address', width: 250 },
         { field: 'phone', headerName: 'Contact Number', width: 150 },
     ];
 
@@ -32,6 +33,16 @@ export default function Clients() {
             <DataGrid
                 rows={users}
                 columns={columns}
+                gridRowId={(row) => row.id}
+                checkboxSelection
+                disableRowSelectionOnClick
+                onRowSelectionModelChange={(newRowSelectionModel) => {
+                    setRowSelectionModel(newRowSelectionModel);
+                }}
+                rowSelectionModel={rowSelectionModel}
+                slots={{
+                    toolbar: GridToolbar,
+                }}
                 initialState={{ pagination: { page: 0, pageSize: 10 } }}
                 pageSizeOptions={[5, 10]}
             />
